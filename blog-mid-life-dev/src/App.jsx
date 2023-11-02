b2ed4dc35b85682ee21a3fa1423fc41305f25816
tom-gora <goratomasz@outlook.com>
2023-11-02 23:33:44 +0000

    theme and navbar done

--- blog-mid-life-dev/src/App.jsx
+++ blog-mid-life-dev/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
 import Navbar from "./layouts/Navbar";
 import Header from "./layouts/Header";
 import SectionSliderRecentPosts from "./layouts/SectionSliderRecentPosts";
@@ -7,14 +8,49 @@ import SectionAllPosts from "./layouts/SectionAllPosts";
 import Post from "./layouts/Post";
 import Footer from "./layouts/Footer";
 
-function App() {
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [showNavigationShadow, setShowNavigationShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 20) {
+        // User has scrolled 200 pixels from the top, so show the shadow.
+        setShowNavigationShadow(true);
+      } else {
+        // User is within the top 200 pixels, so hide the shadow.
+        setShowNavigationShadow(false);
+      }
+    };
+
+    // Attach the scroll event listener when the component mounts.
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener when the component unmounts to avoid memory leaks.
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
-    <div className="main dark">
-      <div className="test">test</div>
-      <Navbar />
+    <div className={`main ${isDarkMode ? 'dark' : ''} `}>
+      <Navbar toggleDarkMode={toggleDarkMode} showNavigationShadow={showNavigationShadow} isDarkMode={isDarkMode} />
+      <div style={{ paddingTop: '10rem', minHeight: '1000px', padding: '20px', fontSize: '16px', lineHeight: '1.5', overflowY: 'auto' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ipsum id mauris ultrices, eget tincidunt tellus auctor. Nullam nec eros nec justo fringilla auctor. Suspendisse potenti. Vivamus eget mauris nec dui blandit ullamcorper. Vestibulum sollicitudin, eros id dignissim vestibulum, augue metus facilisis leo, sit amet fringilla ante quam sit amet dolor.
+
+        // ... (repeat the text as needed to create a long scrollable content)
+
+        Vivamus euismod nulla in mi convallis, quis consequat metus elementum. In non lectus at purus lacinia finibus ac vel mi. In ac efficitur tellus, eget tempor ante. Vestibulum auctor tellus vitae risus bibendum luctus. Sed dignissim libero eu nulla sollicitudin, eu mollis arcu posuere. Sed vestibulum sem at ante condimentum, at feugiat turpis elementum.
+
+      // ... (repeat the text as needed to create a long scrollable content)
+      </div>
+
       <Header />
       <SectionSliderRecentPosts />
-      <SectionAllPosts />
+      <SectionAllPosts /> w
       <Post />
       <Footer />
     </div>
--- blog-mid-life-dev/src/layouts/Navbar.jsx
+++ blog-mid-life-dev/src/layouts/Navbar.jsx
@@ -1,5 +1,35 @@
-const Navbar = () => {
-  return (null);
-}
+import "./styles/Navbar.css";
+import { IoMoonOutline } from "react-icons/io5";
+import { IoSunnyOutline } from "react-icons/io5";
+import { GiPlainCircle } from "react-icons/gi";
+
+const Navbar = ({ isDarkMode, toggleDarkMode, showNavigationShadow }) => {
+  return (
+    <div className="navigation" style={showNavigationShadow ? { boxShadow: '0 0 5px var(--shadow-opaque)' } : {}}>
+      <div className="name">Tomasz Gora</div>
+      <div className="navigation-segment">
+        <ul className="navigation-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+        </ul>
+        <label htmlFor="theme-input">
+          <input type="checkbox" id="theme-input" checked={isDarkMode} onChange={toggleDarkMode} />
+          <div className="toggle-switch">
+            <div>{<IoSunnyOutline />}</div>
+            <div> {<GiPlainCircle />}</div>
+            <div>{<IoMoonOutline />}</div>
+          </div>
+        </label>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
