9db3572d153456cb0ab774f0b85565e96b9d7280
tom-gora <goratomasz@outlook.com>
2023-11-04 15:56:45 +0000

    recent posts slider ui finished

--- blog-mid-life-dev/src/layouts/SectionSliderRecentPosts.jsx
+++ blog-mid-life-dev/src/layouts/SectionSliderRecentPosts.jsx
@@ -1,5 +1,69 @@
+import Flickity from "react-flickity-component";
+import { IoChevronBack } from "react-icons/io5"
+import { IoChevronForward } from "react-icons/io5"
+
+import "./styles/SectionSliderRecentPosts.css";
+
 const SectionSliderRecentPosts = () => {
-  return (null);
-}
+  const sliderConfig = {
+    initialIndex: 2,
+    wrapAround: true,
+    autoPlay: 3000,
+    pageDots: false,
+    pauseAutoPlayOnHover: true,
+  };
+
+  return (
+    <div className="recent-posts__slider-wrapper">
+      <h2 className="recent-posts__section_header">Recent posts</h2>
+
+      <Flickity
+        className={"recent-postst__slider"} // default ''
+        elementType={"div"} // default 'div'
+        options={sliderConfig} // takes flickity options {}
+        disableImagesLoaded={false} // default false
+      >
+        <div className="recent-posts__post">
+          <div className="recent-posts__post-date">22 Sep 2024</div>
+          <h3 className="recent-posts__post-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sequi, omnis.
+          </h3>
+          <img
+            className="recent-posts__post-image"
+            src="https://images.unsplash.com/photo-1696451203476-7ee3bbfc882e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=720&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5OTEwMzAxNw&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+          />
+        </div>
+
+        <div className="recent-posts__post">
+          <div className="recent-posts__post-date">22 Sep 2024</div>
+          <h3 className="recent-posts__post-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sequi, omnis.
+          </h3>
+          <img
+            className="recent-posts__post-image"
+            src="https://images.unsplash.com/photo-1696451203476-7ee3bbfc882e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=720&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5OTEwMzAxNw&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+          />
+        </div>
+
+        <div className="recent-posts__post">
+          <div className="recent-posts__post-date">22 Sep 2024</div>
+          <h3 className="recent-posts__post-title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sequi, omnis.
+          </h3>
+          <img
+            className="recent-posts__post-image"
+            src="https://images.unsplash.com/photo-1696451203476-7ee3bbfc882e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=720&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5OTEwMzAxNw&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+          />
+        </div>
+      </Flickity>
+    </div>
+  );
+};
 
 export default SectionSliderRecentPosts;
