f3ee8cd952153644ab9d4cf3082f6dbbc272bb6e
tom-gora <goratomasz@outlook.com>
2025-02-19 12:15:10 +0000

    back link fixed

--- blog-mid-life-dev/src/layouts/Navbar.jsx
+++ blog-mid-life-dev/src/layouts/Navbar.jsx
@@ -2,70 +2,70 @@ import "./styles/Navbar.css";
 import ThemeToggle from "../components/ThemeToggle";
 
 const Navbar = ({
-  setIsMobileMenuOpen,
-  isMobileView,
-  isDarkMode,
-  toggleDarkMode,
-  showNavigationShadow,
-  isMobileMenuOpen,
-  onAboutClick,
-  onHomeClick,
-  currentView,
+    setIsMobileMenuOpen,
+    isMobileView,
+    isDarkMode,
+    toggleDarkMode,
+    showNavigationShadow,
+    isMobileMenuOpen,
+    onAboutClick,
+    onHomeClick,
+    currentView,
 }) => {
-  return (
-    <div
-      className="navbar"
-      style={{
-        ...(showNavigationShadow
-          ? { boxShadow: "0 0 5px var(--shadow-opaque)" }
-          : {}),
-        ...(isMobileMenuOpen ? { right: "0" } : { right: "-120%" }),
-      }}
-    >
-      <div className="navbar__name">Tom Gora</div>
-      <div className="navbar_right-segment">
-        <ul className="navbar__links">
-          <li>
-            <a
-              role="button"
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                setIsMobileMenuOpen(false);
-                currentView !== "home" &&
-                  onHomeClick &&
-                  onHomeClick();
-              }}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              role="button"
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                setIsMobileMenuOpen(false);
-                currentView !== "about" &&
-                  onAboutClick &&
-                  onAboutClick();
-              }}
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a href="https://tomgora.online" role="button">
-              Projects
-            </a>
-          </li>
-        </ul>
-        <ThemeToggle
-          toggleDarkMode={toggleDarkMode}
-          isDarkMode={isDarkMode}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="navbar"
+            style={{
+                ...(showNavigationShadow
+                    ? { boxShadow: "0 0 5px var(--shadow-opaque)" }
+                    : {}),
+                ...(isMobileMenuOpen ? { right: "0" } : { right: "-120%" }),
+            }}
+        >
+            <div className="navbar__name">Tom Gora</div>
+            <div className="navbar_right-segment">
+                <ul className="navbar__links">
+                    <li>
+                        <a
+                            role="button"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => {
+                                setIsMobileMenuOpen(false);
+                                currentView !== "home" &&
+                                    onHomeClick &&
+                                    onHomeClick();
+                            }}
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            role="button"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => {
+                                setIsMobileMenuOpen(false);
+                                currentView !== "about" &&
+                                    onAboutClick &&
+                                    onAboutClick();
+                            }}
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://tomgora.online" role="button">
+                            Portfolio ⮐
+                        </a>
+                    </li>
+                </ul>
+                <ThemeToggle
+                    toggleDarkMode={toggleDarkMode}
+                    isDarkMode={isDarkMode}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
